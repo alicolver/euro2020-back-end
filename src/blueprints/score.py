b1311df4db264303255c41e7f79dd6bebbcbd134
Ali Colver <me@alicolver.com>
2021-06-10 22:32:17 +0100

    Adding in getLiveGames

--- src/blueprints/score.py
+++ src/blueprints/score.py
@@ -1,12 +1,63 @@
+from datetime import timedelta
+import datetime
 from flask import Blueprint, jsonify, request
 from sqlalchemy.sql import exists
 from database.connection_manager import Session
 from blueprints.authentication import admin_required
 from database.orm import Match, Prediction
+from blueprints.predictions import check_kicked_off
 session = Session()
 
 
 scores = Blueprint('scores', __name__)
+
+
+@scores.route('/get-live-games', methods=['get'])
+@admin_required
+def getLiveGames():
+    today = datetime.today()
+    today = datetime(today.year, today.month, today.day, today.hours, today.seconds)
+
+    matches = session.query(Match).filter(
+        Match.match_date == today).filter(not Match.is_fulltime).all()
+
+    results = []
+
+    for match in matches:
+        matchid = getattr(match, 'matchid')
+        if not check_kicked_off(matchid):
+            continue
+
+        team_one_id = getattr(match, 'team_one_id')
+        team_two_id = getattr(match, 'team_two_id')
+
+        team_one = session.query(Team).filter(Team.teamid == team_one_id)[0]
+        team_two = session.query(Team).filter(Team.teamid == team_two_id)[0]
+
+        match_formated = {
+            "team_one": {
+                "name": getattr(team_one, 'name'),
+                "emoji": getattr(team_one, 'emoji')
+            },
+            "team_two": {
+                "name": getattr(team_two, 'name'),
+                "emoji": getattr(team_two, 'emoji')
+            },
+            "match": {
+                "match_date": getattr(match, 'match_date').isoformat(),
+                "kick_off_time": getattr(match, 'kick_off_time').isoformat(),
+                "is_knockout": getattr(match, 'is_knockout'),
+                "matchid": matchid
+            }
+        }
+
+        results.append(match_formated)
+
+    return jsonify({
+        "success": True,
+        "matches": results
+    })
+
 
 
 @scores.route('/score', methods=['put'])
