e3165d4a013ddb977e628707f960efef69f3f985
Luke Ely <le2417@ic.ac.uk>
2021-06-10 22:04:08 +0100

    Sorry server :(

--- src/blueprints/authentication.py
+++ src/blueprints/authentication.py
@@ -144,7 +144,7 @@ def validateToken(userid):
 
 @authentication.route('/is-admin', methods=["GET"])
 @admin_required
-def validateToken(userid):
+def isAdmin(userid):
     return jsonify({
         'success': True
     })
