14c191c48515c2bdc5ea8b1e2e1d6e979c3a4db5
Luke Ely <le2417@ic.ac.uk>
2021-06-11 18:54:52 +0100

    Add is_user field to leaderboard objects

--- src/blueprints/leaderboard.py
+++ src/blueprints/leaderboard.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request
 from sqlalchemy.sql import exists
 from database.connection_manager import Session
 from database.orm import User
+from blueprints.authentication import auth_required
 from blueprints.score import calculate_user_score
 session = Session()
 
@@ -10,16 +11,19 @@ leaderboard = Blueprint('leaderboard', __name__)
 
 
 @leaderboard.route('/leaderboard', methods=['GET'])
-def getLeaderboard():
+@auth_required()
+def getLeaderboard(userid):
     users = session.query(User).filter(User.hidden == False).all()
     users_formated = []
     for user in users:
         score, correct_scores, correct_results = calculate_user_score(user)
+        is_user = getattr(user, 'userid') == userid
         user_formated = {
             "name": getattr(user, 'name'),
             "score": score,
             "correct_scores": correct_scores,
             "correct_results": correct_results,
+            "is_user": is_user,
         }
         users_formated.append(user_formated)
 
