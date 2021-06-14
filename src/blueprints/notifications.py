810159e27bb30483ce9a41d0d97153ff26b9fa68
Luke Ely <le2417@ic.ac.uk>
2021-06-14 16:46:12 +0100

    Add subject to missing prediction emails

--- src/blueprints/notifications.py
+++ src/blueprints/notifications.py
@@ -23,6 +23,7 @@ You stil have 1 hour to submit your prediction by visiting www.alicolver.com/eur
 Good luck!
 """
 
+
 @notifications.route('/check_for_predictions', methods=['GET'])
 def check_upcoming_games():
 
@@ -31,7 +32,7 @@ def check_upcoming_games():
     today = datetime(today.year, today.month, today.day)
 
     matches = session.query(Match).filter(Match.match_date == today).all()
-    
+
     for match in matches:
         kick_off = getattr(match, 'kick_off_time')
         match_date = getattr(match, 'match_date')
@@ -40,24 +41,25 @@ def check_upcoming_games():
 
         current_time = datetime.now(timezone)
 
-        if combined < (current_time + timedelta(hours = 1)) and not check_kicked_off(match.matchid):
+        if combined < (current_time + timedelta(hours=1)) and not check_kicked_off(match.matchid):
             check_user_prediction(match)
     return jsonify({"message": "no games in the next hour"})
 
+
 def check_user_prediction(match):
-    #users = session.query(User).filter(User.hidden == False).all()
     users = session.query(User).filter(User.hidden == False).all()
     for user in users:
         if not session.query(exists().where(Prediction.userid == user.userid).where(Prediction.matchid == match.matchid)).scalar():
             send_email_reminder(user, match)
     return jsonify({"message": "Reminder emails sent"})
 
+
 def send_email_reminder(user, match):
     email = user.email
-    msg = Message("Subject", sender = "euros2020predictions@gmail.com", recipients = [email])
+    msg = Message("Mising Prediction",
+                  sender="euros2020predictions@gmail.com", recipients=[email])
     msg.body = email_message.format(
-        user = user.name,
-        game = "{} vs {}".format(match.team_one.name, match.team_two.name)
+        user=user.name,
+        game="{} vs {}".format(match.team_one.name, match.team_two.name)
     )
     mail_object.send(msg)
-    
